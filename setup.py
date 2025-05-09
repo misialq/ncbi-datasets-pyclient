6d3d2e85d1a65e628471d1702fe7da584b51b685
Michal Ziemski <ziemski.michal@gmail.com>
2025-05-09 07:07:52 +0000

    Update code files for release v18.1.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "ncbi-datasets-pyclient"
-VERSION = "v18.0.5"
+VERSION = "v18.1.0"
 PYTHON_REQUIRES = ">= 3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 3.0.0",
