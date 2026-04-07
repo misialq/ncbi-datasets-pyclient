b9f9b15fe42925f16abeeb43e891cdefb752050f
BOTulich <bokulich-lab@ethz.ch>
2026-04-07 07:33:47 +0000

    Update code files for release v18.23.0

--- ncbi/datasets/openapi/__init__.py
+++ ncbi/datasets/openapi/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "v18.22.1"
+__version__ = "v18.23.0"
 
 # Define package exports
 __all__ = [
--- ncbi/datasets/openapi/configuration.py
+++ ncbi/datasets/openapi/configuration.py
@@ -564,7 +564,7 @@ conf = ncbi.datasets.openapi.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v2\n"\
-               "SDK Package Version: v18.22.1".\
+               "SDK Package Version: v18.23.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
